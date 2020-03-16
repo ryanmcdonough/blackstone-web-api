a8a2ddf3cbb2a5dcc3285a6d831aa5c670890744
ryanmcdonough <me@ryanmcdonough.co.uk>
2020-03-16 11:52:24 +0000

    Update server.py
    Move to port 80

--- server.py
+++ server.py
@@ -23,4 +23,4 @@ class SpacyCoreNlp(Resource):
 api.add_resource(SpacyCoreNlp, '/core-nlp')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
