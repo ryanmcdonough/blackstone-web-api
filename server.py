f14b1649293469761a519fd0c386a951d0b215af
ryanmcdonough <me@ryanmcdonough.co.uk>
2020-03-16 11:47:49 +0000

    Create server.py
    Added endpoint for legislation linker.

---
+++ server.py
@@ -0,0 +1,26 @@
+import os
+import spacy
+from flask import Flask, request
+from flask_restful import Resource, Api
+from blackstone.legislation_linker import extract_legislation_relations
+
+nlp = spacy.load("en_blackstone_proto")
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class SpacyCoreNlp(Resource):
+        def legislation(self):
+        query = request.data.decode(request.charset)
+        doc = nlp(text) 
+        relations = extract_legislation_relations(doc)
+
+        return relations
+
+
+
+api.add_resource(SpacyCoreNlp, '/core-nlp')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
