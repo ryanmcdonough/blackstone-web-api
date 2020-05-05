f45b13c2ee4cbeb830f213faa8477567de038bb2
McDonough, Ryan <MCDOR@addleshawgoddard.com>
2020-05-05 09:16:28 +0100

    Added Entity & Legislation classes, updated API method to return those

--- server.py
+++ server.py
@@ -1,31 +1,40 @@
 import os
+import jsonpickle
 import spacy
 from flask import Flask, request
 from flask_restful import Resource, Api
+from blackstone.utils.legislation_linker import extract_legislation_relations
+from Legislation import Legislation
+
 
 nlp = spacy.load("en_blackstone_proto")
 
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/ner', methods=['POST'])
-def ner(self):
+@app.route('/legislation', methods=['POST'])
+def ner():
     req_data = request.get_json()
     text = req_data['text']
                 
     doc = nlp(text) 
+    relations = extract_legislation_relations(doc)
 
-    return doc.ents
+    legislations = []
 
-@app.route("/test")
-def test(self):
-    req_data = request.get_json()
-    text = req_data['text']
-                
-    doc = nlp(text) 
+    for provision, provision_url, instrument, instrument_url in relations:
+        legislations.append(Legislation(provision.text,provision_url,instrument.text,instrument_url))
 
-    return doc.ents
+
+    return jsonpickle.encode(legislations, unpicklable=False)
+
+
+@app.route('/status')
+def hello():
+    # Render the page
+    return "Working"
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='localhost', port=4449)
